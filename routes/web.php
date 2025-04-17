ec0d44f6dd876219b2aafc1a9469b149fcd8f35d
birukashagre6 <mandave693@gmail.com>
2025-04-17 22:14:32 +0300

    Adding routes for welcome page and language switching

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('lang/{locale}', function ($locale) {
-    session()->put('locale', $locale);
-    app()->setLocale($locale);
+Route::get('/lang/{locale}', function ($locale) {
+    $allowed = ['en', 'am', 'om', 'so', 'ti'];
+    if (in_array($locale, $allowed)) {
+        Session::put('locale', $locale);
+    }
     return redirect()->back();
 });
+
