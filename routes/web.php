63d1f08cde25a2537f2b09339b7a35010241e6b4
birukashagre6 <mandave693@gmail.com>
2025-04-17 21:51:36 +0300

    Created SetLocale middleware to manage app language from session

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('lang/{locale}', function ($locale) {
+    session()->put('locale', $locale);
+    app()->setLocale($locale);
+    return redirect()->back();
+});
